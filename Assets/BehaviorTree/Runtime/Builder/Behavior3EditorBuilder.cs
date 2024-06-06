b2fdc57de24d3d0894e5a349cef8e08377d5560a
袁文滔 <15wtyuan@gmail.com>
2024-06-06 11:33:02 +0800

    :truck:refactor:代码规范

--- Assets/BehaviorTree/Runtime/Builder/Behavior3EditorBuilder.cs
+++ Assets/BehaviorTree/Runtime/Builder/Behavior3EditorBuilder.cs
@@ -5,45 +5,6 @@ using MiniJSON;
 
 namespace BehaviorTree.Runtime
 {
-    public static class MiniJsonHelper
-    {
-        public static int ParseInt(object value)
-        {
-            return value switch
-            {
-                long l => (int)l,
-                string s => int.Parse(s),
-                _ => throw new FormatException()
-            };
-        }
-
-        public static float ParseFloat(object value)
-        {
-            return value switch
-            {
-                double d => (float)d,
-                long l => l,
-                string s => float.Parse(s),
-                _ => throw new FormatException()
-            };
-        }
-
-        public static bool ParseBool(object value)
-        {
-            return value switch
-            {
-                bool b => b,
-                string s => bool.Parse(s),
-                _ => throw new FormatException()
-            };
-        }
-
-        public static string ParseString(object value)
-        {
-            return value.ToString();
-        }
-    }
-
     /// <summary>
     /// https://github.com/behavior3/behavior3editor
     /// from json
---
+++ Assets/BehaviorTree/Runtime/Builder/MiniJsonHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BehaviorTree.Runtime
+{
+    public static class MiniJsonHelper
+    {
+        public static int ParseInt(object value)
+        {
+            return value switch
+            {
+                long l => (int)l,
+                string s => int.Parse(s),
+                _ => throw new FormatException()
+            };
+        }
+
+        public static float ParseFloat(object value)
+        {
+            return value switch
+            {
+                double d => (float)d,
+                long l => l,
+                string s => float.Parse(s),
+                _ => throw new FormatException()
+            };
+        }
+
+        public static bool ParseBool(object value)
+        {
+            return value switch
+            {
+                bool b => b,
+                string s => bool.Parse(s),
+                _ => throw new FormatException()
+            };
+        }
+
+        public static string ParseString(object value)
+        {
+            return value.ToString();
+        }
+    }
+}
